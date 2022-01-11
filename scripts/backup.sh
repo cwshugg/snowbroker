b2da6cffd434ce95210e4079c89eac368ece9553
Connor Shugg <cwshugg@vt.edu>
2022-01-11 15:58:44 +0000

    wrote script to backup strategy work to somewhere else

---
+++ scripts/backup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Script to copy a given directory (used by strategies to store data) elsewhere
+# (presumably to some location where it'll get saved).
+# Backups are recurring, and this script runs forever.
+
+# get the backup directory from the command-line args
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 /path/to/source_dir /path/to/dest_dir"
+    exit 1
+fi
+sdir=$1
+ddir=$2
+
+sleep_time=1800
+
+# enter the backup look
+while true; do
+    # make sure both directories are valid
+    if [ ! -d ${sdir} ]; then
+        echo "Source directory (${sdir}) is not a directory."
+        exit 1
+    elif [ ! -d ${ddir} ]; then
+        echo "Destination directory (${ddir}) is not a directory."
+        exit 1
+    fi
+
+    # copy from one to another
+    echo -e "[$(date)] backing up...\033[90m"
+    cp -v -r ${sdir}/* ${ddir}/
+
+    # sleep for a time
+    echo -e "\033[0m[$(date)] going to sleep..."
+    sleep ${sleep_time}
+done
+
